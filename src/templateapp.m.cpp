c29f316e168796dd6218d2a74e4271904d14c99a
Hunter LaTourette <hlatourette@gmail.com>
2025-03-30 20:47:10 -0400

    Correct indentation

--- src/templateapp.m.cpp
+++ src/templateapp.m.cpp
@@ -7,9 +7,9 @@ using namespace templateapp;
 
 int main()
 {
-  std::string question;
-  std::cout << "What would you like to know?" << std::endl;
-  std::cin >> question;
-  std::cout << meaningOfLifeTheUniverseAndEverything(question) << std::endl;
-  return 0;
+    std::string question;
+    std::cout << "What would you like to know?" << std::endl;
+    std::cin >> question;
+    std::cout << meaningOfLifeTheUniverseAndEverything(question) << std::endl;
+    return 0;
 }
