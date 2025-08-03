0deb79a3bfe0acee0dd3fe9a94c097e913a68e8a
Hunter LaTourette <hlatourette@gmail.com>
2025-08-02 23:11:54 -0400

    update args (#17)

--- src/templateapp.m.cpp
+++ src/templateapp.m.cpp
@@ -5,8 +5,13 @@
 
 using namespace templateapp;
 
-int main()
+int main(int argc, char* argv[])
 {
+    if (argc < 1) {
+        std::cerr << "Usage: " << argv[0] << std::endl;
+        return 1;
+    }
+
     std::string question;
     std::cout << "What would you like to know?" << std::endl;
     std::cin >> question;
